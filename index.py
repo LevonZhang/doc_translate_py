4e2fa52e0d54f4de39bedcf013c60da4c772f589
LevonZhang <144426503+LevonZhang@users.noreply.github.com>
2024-08-30 11:06:02 +0800

    Update index.py

--- index.py
+++ index.py
@@ -198,9 +198,9 @@ async def process_paragraph(paragraph, translations, paragraph_index):
                 if translated_text:
                     paragraph.text = translated_text
             except IndexError as e:
-                st.error(f"替换翻译文本出现错误：{e}")
-                st.error(f"paragraph_index: {paragraph_index}, len(translations): {len(translations)}, translations: {translations}")  # 打印调试信息
-                st.exception(e)
+                st.error(f"替换翻译文本出现错误：{e} \n paragraph_index: {paragraph_index}, \n translations: {translations}")
+                # st.error(f", len(translations): {len(translations)}, ")  # 打印调试信息
+                # st.exception(e)
             if run:
                for new_run in paragraph.runs:
                    new_run.font.bold = run.font.bold
