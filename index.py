9331e0c349008f9ac04559a27cd898c1cee4b562
LevonZhang <144426503+LevonZhang@users.noreply.github.com>
2024-09-02 03:28:50 +0000

    hide the GitHub icon

--- index.py
+++ index.py
@@ -9,16 +9,13 @@ import typing_extensions as typing
 
 # Add custom CSS to hide the GitHub icon
 st.markdown(
-    """
+    r"""
     <style>
-    .css-1jc7ptx, .e1ewe7hr3, .viewerBadge_container__1QSob,
-    .styles_viewerBadge__1yB5_, .viewerBadge_link__1S137,
-    .viewerBadge_text__1JaDK {
-        display: none;
-    }
+        [data-testid="stStatusWidget"] {
+            visibility: hidden;
+        }
     </style>
-    """,
-    unsafe_allow_html=True
+    """, unsafe_allow_html=True
 )
 
 # --- 全局变量 ---
